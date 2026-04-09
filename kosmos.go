e80c6505d877b2b925179152d187fe085a1a8e23
Ed <me@mypierian.com>
2026-04-09 16:26:22 -0700

    update

--- kosmos.go
+++ kosmos.go
@@ -8,6 +8,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Ignite Base the kosmos where Secret Manager is optional and no Observer Client is created.
+// Failure to do so will be fatal to the application.  An application cannot exist without the kosmos.
+func IgniteBase(cmdSource *cobra.Command, source ...string) {
+	ether.UniversalConstants.MergeFromFile(source...)
+	if cmdSource != nil {
+		ether.UniversalConstants.MergeFromCmd(cmdSource)
+	}
+
+	projectId := ether.UniversalConstants.Collapse().ProjectID
+	if projectId != "" {
+		if err := ether.LoadSecretsFile(&ether.GCPSecretManager{ProjectID: projectId}); err != nil {
+			log.Fatalf("Fatal: Failed to load secrets file: %v", err)
+		}
+	}
+
+	if err := ether.CollapseKnownEthers(cmdSource, source...); err != nil {
+		log.Fatalf("Fatal: Failed to collapse known ethers: %v", err)
+	}
+
+}
+
 // Ignite the kosmos.  Failure to do so will be fatal to the application.  An application cannot exist without the kosmos.
 func Ignite(cmdSource *cobra.Command, source ...string) {
 	ether.UniversalConstants.MergeFromFile(source...)
